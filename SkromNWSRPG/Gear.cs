bb2344ebc22cfe37d19cc8adec3bc0a14299552b
CT <thomasulrac@gmail.com>
2021-09-21 15:20:14 +0200

    CheckFields & CheckGear & IsAbstract validé

--- SkromNWSRPG/Gear.cs
+++ SkromNWSRPG/Gear.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 namespace SkromNWSRPG
 {
     /*
-     * Cette classe représente un équippement dans notre RPG
-     * Il possède un emplacement d'équipement
+     * Cette classe représente un équipement dans notre RPG
+     * Il possède un emplacement d'équipement var Slot de type GearSlot
      * C'est une classe Abstraite
      * Elle hérite de la classe Item
      */
-    public abstract class Gear
+    public abstract class Gear : Item
     {
+        public GearSlot Slot;
     }
 }
